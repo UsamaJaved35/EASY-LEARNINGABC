d19c6e7040a8acf5cb862d70dcdbfd5a16da6d6e
Usama <usamamughal4949@gmail.com>
2022-05-22 03:23:34 +0500

    implemented backend for Learning page 1

--- app/src/main/java/com/example/easylearingabc/LearninPage1.java
+++ app/src/main/java/com/example/easylearingabc/LearninPage1.java
@@ -3,12 +3,124 @@ package com.example.easylearingabc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LearninPage1 extends AppCompatActivity {
-
+public class LearninPage1 extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learnin_page1);
+        Button A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,r,S,T,U,V,W,X,Y,Z;
+        A=findViewById(R.id.A);
+        B=findViewById(R.id.B);
+        C=findViewById(R.id.C);
+        D=findViewById(R.id.D);
+        E=findViewById(R.id.E);
+        F=findViewById(R.id.F);
+        G=findViewById(R.id.G);
+        H=findViewById(R.id.H);
+        I=findViewById(R.id.I);
+        J=findViewById(R.id.J);
+        K=findViewById(R.id.K);
+        L=findViewById(R.id.L);
+        M=findViewById(R.id.M);
+        N=findViewById(R.id.N);
+        O=findViewById(R.id.O);
+        P=findViewById(R.id.P);
+        Q=findViewById(R.id.Q);
+        r=findViewById(R.id.R);
+        S=findViewById(R.id.S);
+        T=findViewById(R.id.T);
+        U=findViewById(R.id.U);
+        V=findViewById(R.id.V);
+        W=findViewById(R.id.W);
+        X=findViewById(R.id.X);
+        Y=findViewById(R.id.Y);
+        Z=findViewById(R.id.Z);
+        A.setOnClickListener(this);
+        B.setOnClickListener(this);
+        C.setOnClickListener(this);
+        D.setOnClickListener(this);
+        E.setOnClickListener(this);
+        F.setOnClickListener(this);
+        G.setOnClickListener(this);
+        H.setOnClickListener(this);
+        I.setOnClickListener(this);
+        J.setOnClickListener(this);
+        K.setOnClickListener(this);
+        L.setOnClickListener(this);
+        M.setOnClickListener(this);
+        N.setOnClickListener(this);
+        O.setOnClickListener(this);
+        P.setOnClickListener(this);
+        Q.setOnClickListener(this);
+        r.setOnClickListener(this);
+        S.setOnClickListener(this);
+        T.setOnClickListener(this);
+        U.setOnClickListener(this);
+        V.setOnClickListener(this);
+        W.setOnClickListener(this);
+        X.setOnClickListener(this);
+        Y.setOnClickListener(this);
+        Z.setOnClickListener(this);
+    }
+    public void onClick(View v)
+    {
+        switch (v.getId()){
+            case R.id.A:
+                break;
+            case R.id.B:
+                break;
+            case R.id.C:
+                break;
+            case R.id.D:
+                break;
+            case R.id.E:
+                break;
+            case R.id.F:
+                break;
+            case R.id.G:
+                break;
+            case R.id.H:
+                break;
+            case R.id.I:
+                break;
+            case R.id.J:
+                break;
+            case R.id.K:
+                break;
+            case R.id.L:
+                break;
+            case R.id.M:
+                break;
+            case R.id.N:
+                break;
+            case R.id.O:
+                break;
+            case R.id.P:
+                break;
+            case R.id.Q:
+                break;
+            case R.id.R:
+                break;
+            case R.id.S:
+                break;
+            case R.id.T:
+                break;
+            case R.id.U:
+                break;
+            case R.id.V:
+                break;
+            case R.id.W:
+                break;
+            case R.id.X:
+                break;
+            case R.id.Y:
+                break;
+            case R.id.Z:
+                break;
+
+        }
     }
 }
