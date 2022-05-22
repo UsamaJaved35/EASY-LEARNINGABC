1d773bb70c2bca5a01c31ad934732e53f0165599
Usama <usamamughal4949@gmail.com>
2022-05-22 10:59:44 +0500

    some changes in backend for learning page 1

--- app/src/main/java/com/example/easylearingabc/LearninPage1.java
+++ app/src/main/java/com/example/easylearingabc/LearninPage1.java
@@ -2,6 +2,7 @@ package com.example.easylearingabc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -67,60 +68,87 @@ public class LearninPage1 extends AppCompatActivity implements View.OnClickListe
     }
     public void onClick(View v)
     {
+        Intent sendimage = new Intent(LearninPage1.this, LearningPage2.class);
         switch (v.getId()){
             case R.id.A:
+                sendimage.putExtra("image", R.drawable.A);
                 break;
             case R.id.B:
+                sendimage.putExtra("image", R.drawable.B);
                 break;
             case R.id.C:
+                sendimage.putExtra("image", R.drawable.C);
                 break;
             case R.id.D:
+                sendimage.putExtra("image", R.drawable.D);
                 break;
             case R.id.E:
+                sendimage.putExtra("image", R.drawable.E);
                 break;
             case R.id.F:
+                sendimage.putExtra("image", R.drawable.F);
                 break;
             case R.id.G:
+                sendimage.putExtra("image", R.drawable.G);
                 break;
             case R.id.H:
+                sendimage.putExtra("image", R.drawable.H);
                 break;
             case R.id.I:
+                sendimage.putExtra("image", R.drawable.I);
                 break;
             case R.id.J:
+                sendimage.putExtra("image", R.drawable.J);
                 break;
             case R.id.K:
+                sendimage.putExtra("image", R.drawable.K);
                 break;
             case R.id.L:
+                sendimage.putExtra("image", R.drawable.L);
                 break;
             case R.id.M:
+                sendimage.putExtra("image", R.drawable.M);
                 break;
             case R.id.N:
+                sendimage.putExtra("image", R.drawable.N);
                 break;
             case R.id.O:
+                sendimage.putExtra("image", R.drawable.O);
                 break;
             case R.id.P:
+                sendimage.putExtra("image", R.drawable.P);
                 break;
             case R.id.Q:
+                sendimage.putExtra("image", R.drawable.Q);
                 break;
             case R.id.R:
+                sendimage.putExtra("image", R.drawable.R);
                 break;
             case R.id.S:
+                sendimage.putExtra("image", R.drawable.S);
                 break;
             case R.id.T:
+                sendimage.putExtra("image", R.drawable.T);
                 break;
             case R.id.U:
+                sendimage.putExtra("image", R.drawable.U);
                 break;
             case R.id.V:
+                sendimage.putExtra("image", R.drawable.V);
                 break;
             case R.id.W:
+                sendimage.putExtra("image", R.drawable.W);
                 break;
             case R.id.X:
+                sendimage.putExtra("image", R.drawable.X);
                 break;
             case R.id.Y:
+                sendimage.putExtra("image", R.drawable.Y);
                 break;
             case R.id.Z:
+                sendimage.putExtra("image", R.drawable.Z);
                 break;
-
         }
+        startActivity(sendimage);
     }
 }
