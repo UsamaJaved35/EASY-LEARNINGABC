5f97a662dd230c6a1cf629c36f98bfc9d9b9ea8d
Usama <usamamughal4949@gmail.com>
2022-06-05 12:32:09 +0500

    some changes in learning layout

--- app/src/main/java/com/example/easylearingabc/LearninPage1.java
+++ app/src/main/java/com/example/easylearingabc/LearninPage1.java
@@ -7,64 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LearninPage1 extends AppCompatActivity implements View.OnClickListener {
+public class LearninPage1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learnin_page1);
-        Button A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,r,S,T,U,V,W,X,Y,Z;
-        A=findViewById(R.id.A);
-        B=findViewById(R.id.B);
-        C=findViewById(R.id.C);
-        D=findViewById(R.id.D);
-        E=findViewById(R.id.E);
-        F=findViewById(R.id.F);
-        G=findViewById(R.id.G);
-        H=findViewById(R.id.H);
-        I=findViewById(R.id.I);
-        J=findViewById(R.id.J);
-        K=findViewById(R.id.K);
-        L=findViewById(R.id.L);
-        M=findViewById(R.id.M);
-        N=findViewById(R.id.N);
-        O=findViewById(R.id.O);
-        P=findViewById(R.id.P);
-        Q=findViewById(R.id.Q);
-        r=findViewById(R.id.R);
-        S=findViewById(R.id.S);
-        T=findViewById(R.id.T);
-        U=findViewById(R.id.U);
-        V=findViewById(R.id.V);
-        W=findViewById(R.id.W);
-        X=findViewById(R.id.X);
-        Y=findViewById(R.id.Y);
-        Z=findViewById(R.id.Z);
-        A.setOnClickListener(this);
-        B.setOnClickListener(this);
-        C.setOnClickListener(this);
-        D.setOnClickListener(this);
-        E.setOnClickListener(this);
-        F.setOnClickListener(this);
-        G.setOnClickListener(this);
-        H.setOnClickListener(this);
-        I.setOnClickListener(this);
-        J.setOnClickListener(this);
-        K.setOnClickListener(this);
-        L.setOnClickListener(this);
-        M.setOnClickListener(this);
-        N.setOnClickListener(this);
-        O.setOnClickListener(this);
-        P.setOnClickListener(this);
-        Q.setOnClickListener(this);
-        r.setOnClickListener(this);
-        S.setOnClickListener(this);
-        T.setOnClickListener(this);
-        U.setOnClickListener(this);
-        V.setOnClickListener(this);
-        W.setOnClickListener(this);
-        X.setOnClickListener(this);
-        Y.setOnClickListener(this);
-        Z.setOnClickListener(this);
     }
     public void onClick(View v)
     {
