f1fe44c19ba01f59d35f34bd99b474989c250a79
Usama <usamamughal4949@gmail.com>
2022-06-05 14:24:47 +0500

    Completed listview Click listener in learning page

--- app/src/main/java/com/example/easylearingabc/LearninPage1.java
+++ app/src/main/java/com/example/easylearingabc/LearninPage1.java
@@ -3,7 +3,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -14,6 +16,11 @@ public class LearninPage1 extends AppCompatActivity {
             R.drawable.m2,R.drawable.n2,R.drawable.o2,R.drawable.p2,R.drawable.q2,R.drawable.r2,
             R.drawable.s2,R.drawable.t2,R.drawable.u2,R.drawable.v2,R.drawable.w2,R.drawable.x2,
             R.drawable.y2,R.drawable.z2};
+    int []imgLearn={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f,
+            R.drawable.g,R.drawable.h,R.drawable.i,R.drawable.j,R.drawable.k,R.drawable.l,
+            R.drawable.m,R.drawable.n,R.drawable.o,R.drawable.p,R.drawable.q,R.drawable.r,
+            R.drawable.s,R.drawable.t,R.drawable.u,R.drawable.v,R.drawable.w,R.drawable.x,
+            R.drawable.y,R.drawable.z};
     String[] letters=new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,91 +34,14 @@ public class LearninPage1 extends AppCompatActivity {
         MyViewAdapter adapter = new MyViewAdapter(this, list);
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                Intent sendImage = new Intent(LearninPage1.this, LearningPage2.class);
+                sendImage.putExtra("image",imgLearn[i]);
+                startActivity(sendImage);
+            }
+        });
     }
-//    public void onClick(View v)
-//    {
-//        Intent sendimage = new Intent(LearninPage1.this, LearningPage2.class);
-//        switch (v.getId()){
-//            case R.id.A:
-//                sendimage.putExtra("image", R.drawable.a);
-//                break;
-//            case R.id.B:
-//                sendimage.putExtra("image", R.drawable.b);
-//                break;
-//            case R.id.C:
-//                sendimage.putExtra("image", R.drawable.c);
-//                break;
-//            case R.id.D:
-//                sendimage.putExtra("image", R.drawable.d);
-//                break;
-//            case R.id.E:
-//                sendimage.putExtra("image", R.drawable.e);
-//                break;
-//            case R.id.F:
-//                sendimage.putExtra("image", R.drawable.f);
-//                break;
-//            case R.id.G:
-//                sendimage.putExtra("image", R.drawable.g);
-//                break;
-//            case R.id.H:
-//                sendimage.putExtra("image", R.drawable.h);
-//                break;
-//            case R.id.I:
-//                sendimage.putExtra("image", R.drawable.i);
-//                break;
-//            case R.id.J:
-//                sendimage.putExtra("image", R.drawable.j);
-//                break;
-//            case R.id.K:
-//                sendimage.putExtra("image", R.drawable.k);
-//                break;
-//            case R.id.L:
-//                sendimage.putExtra("image", R.drawable.l);
-//                break;
-//            case R.id.M:
-//                sendimage.putExtra("image", R.drawable.m);
-//                break;
-//            case R.id.N:
-//                sendimage.putExtra("image", R.drawable.n);
-//                break;
-//            case R.id.O:
-//                sendimage.putExtra("image", R.drawable.o);
-//                break;
-//            case R.id.P:
-//                sendimage.putExtra("image", R.drawable.p);
-//                break;
-//            case R.id.Q:
-//                sendimage.putExtra("image", R.drawable.q);
-//                break;
-//            case R.id.R:
-//                sendimage.putExtra("image", R.drawable.r);
-//                break;
-//            case R.id.S:
-//                sendimage.putExtra("image", R.drawable.s);
-//                break;
-//            case R.id.T:
-//                sendimage.putExtra("image", R.drawable.t);
-//                break;
-//            case R.id.U:
-//                sendimage.putExtra("image", R.drawable.u);
-//                break;
-//            case R.id.V:
-//                sendimage.putExtra("image", R.drawable.v);
-//                break;
-//            case R.id.W:
-//                sendimage.putExtra("image", R.drawable.w);
-//                break;
-//            case R.id.X:
-//                sendimage.putExtra("image", R.drawable.x);
-//                break;
-//            case R.id.Y:
-//                sendimage.putExtra("image", R.drawable.y);
-//                break;
-//            case R.id.Z:
-//                sendimage.putExtra("image", R.drawable.z);
-//                break;
-//        }
-//        startActivity(sendimage);
-//    }
 }
